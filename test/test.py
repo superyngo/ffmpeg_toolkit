ee59f16dd2d19219a100f52efa6b47d90ef48297
superyngo <superyngo@gmail.com>
2025-04-02 16:04:31 +0800

    update

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 import os
 import ffmpeg_toolkit
-from ffmpeg_toolkit import FPRenderTasks
+from ffmpeg_toolkit import FF_TASKS, PARTIAL_TASKS
 from pathlib import Path
 from functools import partial
 from typing import Iterable, Sequence, Optional, Callable, Literal, Mapping, TypedDict
@@ -15,7 +15,7 @@ bin_path = current_file_path.parent / "bin"
 os.environ["PATH"] = a = str(bin_path) + os.pathsep + os.environ["PATH"]
 
 
-input_file = Path(r"C:\Users\user\Downloads\IMG_2180.mp4")
+input_file = Path(r"C:\Users\user\Downloads\2031-12-12_1954875155_merged.mkv")
 # input_file = Path(r"C:\Users\user\Downloads\IMG_2078.mp4")
 dir = Path(r"C:\Users\user\AppData\Local\Temp\tmpsn52hpxj")
 
@@ -26,31 +26,59 @@ jumpcut_args = {
     "b1_multiple": 3,
     "b2_multiple": 10.5,
 }
-# ffmpeg_toolkit.FFRenderTasks().jumpcut(**jumpcut_args).render()
-# ffmpeg_toolkit.FFRenderTasks().speedup(input_file=input_file, multiple=5).render()
+portion_method3 = [
+    (1, "remove"),
+    (1, PARTIAL_TASKS.custom()),
+    (
+        2,
+        PARTIAL_TASKS.cut_silence(
+            even_further=PARTIAL_TASKS.cut_motionless(),
+            odd_further=PARTIAL_TASKS.jumpcut(),
+        ),
+    ),
+]
 
+portion_method2 = [
+    (
+        1,
+        PARTIAL_TASKS.cut_silence(
+            even_further=PARTIAL_TASKS.cut_silence_rerender(),
+            odd_further=PARTIAL_TASKS.jumpcut(),
+        ),
+    ),
+    (4, PARTIAL_TASKS.speedup(multiple=5)),
+    (
+        4,
+        PARTIAL_TASKS.partion_video(
+            # portion_method=portion_method3,
+            output_dir=input_file.parent / "can",
+        ),
+    ),
+]
 
-# ffmpeg_toolkit.FFRenderTasks().cut_silence_rerender(input_file).render()
-# ffmpeg_toolkit.cut_silence(input_file)
-# ffmpeg_toolkit.probe_is_valid_video(input_file)
-# ffmpeg_toolkit.FPRenderTasks().is_valid_video(input_file).render()
-# cut_motionless_config = {
-#     "input_file": input_file,
-#     "threshold": 0.005,
-#     "odd_further": ffmpeg_toolkit.PARTIAL_TASKS.custom(),
-# # }
-# # # # ffmpeg_toolkit.cut_motionless(**cut_motionless_config)
-# ffmpeg_toolkit.cut_silence(
-#     input_file,
-#     odd_further=ffmpeg_toolkit.PARTIAL_TASKS.speedup(),
-#     even_further=ffmpeg_toolkit.PARTIAL_TASKS.jumpcut(**jumpcut_args),
-# )
+portion_method1 = [
+    (1, PARTIAL_TASKS.cut_silence_rerender()),
+    (4, PARTIAL_TASKS.speedup(multiple=5)),
+    # (
+    #     4,
+    #     PARTIAL_TASKS.partion_video(
+    #         portion_method=portion_method2,
+    #         output_dir=input_file.parent / "can",
+    #     ),
+    # ),
+]
 
+# ffmpeg_toolkit.PartitionVideo(
+#     input_file=input_file,
+#     output_file=input_file.parent,
+#     portion_method=portion_method1,
+#     output_dir=input_file.parent / "can",
+# ).render()
 
-ffmpeg_toolkit.PartitionVideo(
+ffmpeg_toolkit.FF_TASKS.cut_motionless(
     input_file=input_file,
-    output_file=input_file.parent,
-)
-
-
+    output_file=input_file,
+    even_further=PARTIAL_TASKS.cut_motionless(),
+    odd_further=PARTIAL_TASKS.cut_silence(),
+).render()
 # code.interact(local=globals())
