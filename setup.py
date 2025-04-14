0ee7f3835012d6c7d9a10e01387a0b6eb4b19e72
superyngo <superyngo@gmail.com>
2025-04-14 10:38:18 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ffmpeg_toolkit",
-    version="0.1.9",
+    version="0.2.0",
     author="Wenyang Tai",
     author_email="superyngo@gmail.com",
     description="A toolkit for working with FFmpeg",
